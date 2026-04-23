e124456dc5abf37d6b122c3709493f9491f967b2
ignacio williams <ignacio.williams@alu.frlp.utn.edu.ar>
2026-04-23 17:49:24 -0300

    feat (backend): implementa api rest con endpoint api/health y configuracion de docker con healthcheck

---
+++ backend/app.py
@@ -0,0 +1,39 @@
+import os
+import psycopg2
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+#Variables de entorno
+
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+
+#Coneccion con la BD
+
+def db_connection():
+    return psycopg2.connect(
+        host = DB_HOST,
+        database = DB_NAME,
+        user = DB_USER,
+        password = DB_PASSWORD
+    )
+
+
+#Endpoint para el HEALTHCHECK
+
+@app.route('/api/health', methods=['GET'])
+def health():
+    response = jsonify({"status": "active"})
+    return response, 200
+
+
+if __name__ == '__main__':
+    #La api escucha el puerto 5000
+    app.run(host='0.0.0.0', port=5000)
