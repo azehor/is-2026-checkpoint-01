9a5f6ebbb9fab642d193a1aa2eb7700cb2fd1310
AlfredoEcheverria <alfredoecheverriapalma@gmail.com>
2026-04-23 07:48:59 -0300

    chore(database): asegura que PostgreSQL exista en la inicializacion

--- database/init.sql
+++ database/init.sql
@@ -1,3 +1,6 @@
+SELECT 'CREATE DATABASE postgres' 
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'postgres') \gexec
+
 -- Creacion de la tabla members
 CREATE TABLE members (
     id SERIAL PRIMARY KEY,
@@ -6,13 +9,13 @@ CREATE TABLE members (
     legajo VARCHAR(20) UNIQUE NOT NULL,
     feature VARCHAR(50) NOT NULL,
     servicio VARCHAR(100) NOT NULL,
-    estado VARCHAR(20) NOT NULL,
+    estado VARCHAR(20) NOT NULL
 );
 
 -- Insercion de datos en la tabla members
 INSERT INTO members (nombre, apellido, legajo, feature, servicio, estado) VALUES
 ('Juan Ignacio', 'Piazza', '31402', 'Feature 01', 'Infraestructura Base', 'Activo'),
 ('Ignacio', 'Williams', '33365', 'Feature 02', 'Backend', 'Activo'),
-('Avril', 'Lugo Gonzalez', '33130', 'Feature 03', 'Frontend', 'Activo');
-('Alfredo', 'Echeverría', '32850', 'Feature 04', 'Database', 'Activo');
+('Avril', 'Lugo Gonzalez', '33130', 'Feature 03', 'Frontend', 'Activo'),
+('Alfredo', 'Echeverría', '32850', 'Feature 04', 'Database', 'Activo'),
 ('Nahuel Fabián', 'Fredes Coronilla', '32059', 'Feature 05', 'Portainer', 'Activo');
